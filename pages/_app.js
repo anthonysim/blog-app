f8b00efd959ea952ea5947e5488fd24b8542b503
Anthony Sim <anthonyysim@gmail.com>
2021-06-17 20:01:11 -0700

    initial nextjs setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+// add bootstrap css
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
