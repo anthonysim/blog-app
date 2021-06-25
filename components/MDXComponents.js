0e927e65e546a64b842693719dcf01657be32035
Anthony Sim <anthonyysim@gmail.com>
2021-06-25 13:13:08 -0700

    syntax highlighting

--- components/MDXComponents.js
+++ components/MDXComponents.js
@@ -1,3 +1,7 @@
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
+
+// stylings
 const titleStyleOptions = {
   // textAlign: 'center',
   color: '#d23669',
@@ -7,6 +11,11 @@ const paragraphStyleOptions = {
   fontSize: '20px',
 }
 
+const syntaxStyleOptions = {
+  fontSize: '15px',
+}
+
+// components of mdx files
 const MDXComponents = {
   h1: (props) => {
     return <h1 style={titleStyleOptions} {...props} />
@@ -14,6 +23,14 @@ const MDXComponents = {
   // p: (props) => {
   //   return <p style={paragraphStyleOptions} {...props} />
   // },
+
+  code: ({ children }) => {
+    return (
+      <SyntaxHighlighter language="javascript" style={docco} customStyle={syntaxStyleOptions}>
+        {children}
+      </SyntaxHighlighter>
+    )
+  }
 }
 
 export default MDXComponents;
