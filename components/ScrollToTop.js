fb5b9a7e5b79bc9380fe1368cfc744a82dea1cc0
Anthony Sim <anthonyysim@gmail.com>
2021-07-06 22:38:33 -0700

    scroll to top appear and disappear

--- components/ScrollToTop.js
+++ components/ScrollToTop.js
@@ -1,20 +1,34 @@
+import React, { useState, useEffect } from 'react';
 import { FaChevronUp } from "react-icons/fa";
 import styles from '../styles/ScrollToTop.module.css';
 
 
 export default function ScrollToTop() {
+  const [isVisible, setVisable] = useState(false);
+
+  useEffect(() => {
+    setVisable(false);
+    document.addEventListener("scroll", () => {
+      toggleVisibility();
+    });
+  }, []);
+
+  const toggleVisibility = () => {
+    window.scrollY > 300 ? setVisable(true) : setVisable(false);
+  }
+
   // Set the top cordinate to 0
   // make scrolling smooth
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
-      behavior: "smooth"
+      behavior: 'smooth',
     });
   };
 
   return (
     <div className={styles.scrollUpButton}>
-      <a onClick={scrollToTop} className={styles.button}><FaChevronUp /></a>
+      {isVisible && <a onClick={scrollToTop} className={styles.button}><FaChevronUp /></a>}
     </div>
   )
 }
